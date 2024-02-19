f1a207ca360b802061e6a23976d977a5c4ad5e21
Andres Pardo <jorgeandres.pardorea@gmail.com>
2024-02-19 13:00:15 -0600

    feat: botón saludar a persona

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hola Ch38");
+
+
+/* funcion que envie un saludo con alert */
+function enviarSaludo(){
+    alert("Hola Mundo!");
+}
+
+/**
+ * Pedir el nombre de la persona usando un prompt
+ * Mostrar un saludo y nombre de la persona con alert
+ */
+function enviarSaludoAPersona(){
+    nombre=prompt("Dime tu nombre");
+    alert("Hola "+nombre);
+}
